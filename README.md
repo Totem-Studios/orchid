35115e3d8a08b5f7e87ad7af5aa105fd82bec836
Nenne375 <neo.mannskar@gmail.com>
2024-04-05 05:00:18 +0200

    "Hello, Slint!"
    Co-authored-by: Pontus Henriksson <pontus@henriksson.space>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Orchid-engine
-A game engine written in Lotus using Directx12
+A game engine written in Lotus using Vulkan
+
+Vulkan SDK download: https://vulkan.lunarg.com/sdk/home#windows
+
+Slint: https://slint.dev/  
+
+Crates (Rust packages) - https://crates.io/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-fn main() {
-  println!("Hello, world!");
-  let thing: String = String::from("Hihihihihhi");
+slint::include_modules!();
 
-  println!("{}", thing);
+fn main() {
+    match MainWindow::new() {
+        Ok(ui) => {
+            if let Err(e) = ui.run() {
+                eprintln!("Failed to run the UI: {:?}", e);
+            }
+        },
+        Err(e) => eprintln!("Failed to create MainWindow: {:?}", e),
+    }
 }
